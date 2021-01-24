0a4c1b2aa762b1f6327619e6da4c63ac78751f7d
Ali Haider Shajeeb <75715728+shajeebHaider@users.noreply.github.com>
2021-01-24 17:26:56 +0600

    Migrations and Make Seller Shop foregin Key

--- vendor/admin.py
+++ vendor/admin.py
@@ -3,27 +3,35 @@ from .models import sellerProfile, SellerRegistration
 
 
 # Register your models here.
-class SellerRegistrationAdmin(admin.ModelAdmin):
+class BecomeSellerline(admin.TabularInline):
+    model = sellerProfile
+    fields = ('ShopDetails', 'user','isSeller')
+    can_delete = True
+    extra = 0
+
+
+class SellerProfileAdmin(admin.ModelAdmin):
     list_display = [
 
-        'ShopName',
-        'Phone',
-        'NID',
-        'TradeLicense',
+        'ShopDetails',
+        'user',
         'isSeller'
     ]
 
-    list_filter = ['NID']
-    search_fields = ['ShopName']
+    list_filter = ['user']
+    search_fields = ['ShopDetails']
 
 
-admin.site.register(sellerProfile, SellerRegistrationAdmin)
+
+admin.site.register(sellerProfile, SellerProfileAdmin)
 
 
 class BecomeSellerAdmin(admin.ModelAdmin):
     list_display = [
 
+        'Shop_Logo',
         'ShopName',
+        'Address',
         'Phone',
         'NID',
         'TradeLicense'
@@ -31,6 +39,6 @@ class BecomeSellerAdmin(admin.ModelAdmin):
 
     list_filter = ['NID']
     search_fields = ['ShopName']
-
+    inlines = [BecomeSellerline]
 
 admin.site.register(SellerRegistration, BecomeSellerAdmin)
--- vendor/models.py
+++ vendor/models.py
@@ -1,7 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-
 # Create your models here.
 from django.urls import reverse
 from django.utils.safestring import mark_safe
@@ -22,26 +21,22 @@ class SellerRegistration(models.Model):
     def __str__(self):
         return self.ShopName
 
+    def Shop_Logo(self):
+        return mark_safe('<img src="{}" width="70" height ="70" />'.format(self.ShopLogo.url))
+
+    Shop_Logo.short_description = 'Image'
+    Shop_Logo.allow_tags = True
+
 
 class sellerProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    ShopName = models.CharField(max_length=50, unique=True, verbose_name='Shop Name')
-    Address = models.TextField()
-    Phone = models.CharField(max_length=20, default="+8801xxxxxxxxx", null=False)
-    NID = models.CharField(max_length=50, unique=True)
-    TradeLicense = models.CharField(max_length=50, verbose_name='Trade License', unique=True)
-    NIDImage = models.ImageField(upload_to='Photos', verbose_name='NID Image')
-    TradeImage = models.ImageField(upload_to='Photos', verbose_name='Trade License Image')
+    ShopDetails = models.OneToOneField(SellerRegistration, on_delete=models.CASCADE,null=True)
     isSeller = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.ShopName
+        return self.ShopDetails.ShopName
 
     def get_absolute_url(self):
         return reverse("sellerProfile", kwargs={'slug': self.slug})
 
-    def sellerPhoto(self):
-        return mark_safe('<img src="{}" width="70" height ="70" />'.format(self.image.url))
 
-    sellerPhoto.short_description = 'Image'
-    sellerPhoto.allow_tags = True
